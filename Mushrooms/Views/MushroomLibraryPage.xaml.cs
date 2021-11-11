d196f01b7159d29ec5a2571b4180efd387582f78
Juan Jose Carmona Vilas <juan.vilas@tsunamisolutions.com>
2021-11-11 15:45:13 -0800

    behaviours, converters

--- Mushrooms/Views/MushroomLibraryPage.xaml.cs
+++ Mushrooms/Views/MushroomLibraryPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Mushrooms.ViewModels;
+﻿using Mushrooms.Utils;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,16 +7,16 @@ namespace Mushrooms.Views
   [XamlCompilation(XamlCompilationOptions.Compile)]
   public partial class MushroomLibraryPage : ContentPage
   {
-    private readonly MushroomsVM _mushroomsViewModel;
     public MushroomLibraryPage()
     {
       InitializeComponent();
-      BindingContext = _mushroomsViewModel = new MushroomsVM();
+      this.BindingContext = ViewModelLocator.MushroomLibraryVM;
     }
-    protected override async void OnAppearing()
-    {
-      base.OnAppearing();
-      _mushroomsViewModel.OnAppearing();
-    }
+    //protected override async void OnAppearing()
+    //{
+    //  base.OnAppearing();
+    //  _mushroomsViewModel.OnAppearing();
+    //}
+    //todo to add busy parameter?
   }
 }
