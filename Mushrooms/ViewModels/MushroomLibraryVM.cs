fa1ec5e855e41b5653ac8fd85c63b10136cf837b
Juan Jose Carmona Vilas <juan.vilas@tsunamisolutions.com>
2021-11-11 17:46:08 -0800

    mushroom library page

--- Mushrooms/ViewModels/MushroomLibraryVM.cs
+++ Mushrooms/ViewModels/MushroomLibraryVM.cs
@@ -1,17 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Mushrooms.Models;
 using Mushrooms.Services;
+using Xamarin.Forms;
 
 namespace Mushrooms.ViewModels
 {
-  internal class MushroomLibraryVM
+  internal class MushroomLibraryVM : BaseViewModel
   {
     private MushroomDataService _mushroomDataService;
+    private ObservableCollection<Mushroom> _mushroomList;
+    public ObservableCollection<Mushroom> MushroomList
+    {
+      get => _mushroomList;
+      set
+      {
+        _mushroomList = value;
+        OnPropertyChanged("MushroomList");
+      }
+    }
+    public ICommand PieSelectedCommand { get; }
+    public ICommand AddCommand { get; }
 
     public MushroomLibraryVM(MushroomDataService mushroomDataService)
     {
       _mushroomDataService = mushroomDataService;
+
+      AddCommand = new Command(OnAddCommand);
+     // PieSelectedCommand = new Command<Pie>(OnPieSelectedCommand);
+
+      MushroomList = new ObservableCollection<Mushroom>();
     }
+
+    public async Task OnAppearing()
+    {
+      //IsBusy = true;
+      //todo verify is ok to return void for delegates?
+      MushroomList = new ObservableCollection<Mushroom>(await _mushroomDataService.GetAsync());
+    }
+
+
+    private void OnMushroomSelectedCommand(Mushroom mushroom)
+    {
+      //  _navigationService.NavigateTo("PieDetailView", pie);
+    }
+
+    private void OnAddCommand()
+    {
+      //App.NavigationService.NavigateTo("PieDetailView");
+      // _navigationService.NavigateTo("PieDetailView");
+    }
+    //private void OnPieChanged(PieDetailViewModel sender, Pie pie)
+    //{
+    //  //Pies = new ObservableCollection<Pie>(PieRepository.Pies);
+    //  //Pies = new ObservableCollection<Pie>(App.PieDataServie.GetAllPies());
+    //  Pies = new ObservableCollection<Pie>(_pieDataService.GetAllPies());
+    //}
   }
 }
--- Mushrooms/Views/MushroomLibraryPage.xaml.cs
+++ Mushrooms/Views/MushroomLibraryPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Mushrooms.Utils;
+using Mushrooms.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,16 +8,18 @@ namespace Mushrooms.Views
   [XamlCompilation(XamlCompilationOptions.Compile)]
   public partial class MushroomLibraryPage : ContentPage
   {
+    private MushroomLibraryVM _mushroomLibraryVM;
     public MushroomLibraryPage()
     {
       InitializeComponent();
-      this.BindingContext = ViewModelLocator.MushroomLibraryVM;
+      _mushroomLibraryVM = ViewModelLocator.MushroomLibraryVM;
+      this.BindingContext = _mushroomLibraryVM;
     }
-    //protected override async void OnAppearing()
-    //{
-    //  base.OnAppearing();
-    //  _mushroomsViewModel.OnAppearing();
-    //}
+    protected override async void OnAppearing()
+    {
+      base.OnAppearing();
+      await _mushroomLibraryVM.OnAppearing();
+    }
     //todo to add busy parameter?
   }
 }
